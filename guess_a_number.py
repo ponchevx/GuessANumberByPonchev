ff7c807e9bef7077d750f5ca98920898e91c3307
ponchevx <chiefhehe@gmail.com>
2024-01-23 00:36:40 +0200

    Added 10 attempts limit to the main game project

--- guess_a_number.py
+++ guess_a_number.py
@@ -1,6 +1,7 @@
 import random
 
 computer_move = random.randint(1, 100)
+attempts = 0
 while True:
     player_input = input("Guess the number (1-100): ")
     if not player_input.isdigit():
@@ -8,10 +9,16 @@ while True:
         continue
 
     player_number = int(player_input)
-    if player_number == computer_move:
+    if player_number == computer_move and attempts < 10:
         print("You guess it!")
         break
-    elif player_number > computer_move:
+    elif player_number > computer_move and attempts < 10:
         print("Too High!")
+        attempts += 1
     else:
-        print("Too Low!")
+        if attempts < 10:
+            print("Too Low!")
+        attempts += 1
+    if attempts > 10:
+        print("No more attempts left!")
+        break
